7c84506649d146cf7716122e3692175fe406670f
NatRousseau <nathan.rousseau@viacesi.fr>
2020-12-15 16:03:45 +0100

    Add CRUD + gitignore

---
+++ CICD-API/Entity/Game.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CICD_API.Entity
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Genre { get; set; }
+    }
+}

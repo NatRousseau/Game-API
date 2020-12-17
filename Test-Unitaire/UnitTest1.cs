20e7c7e1f5afb60040f6a354a60fdba9a3fe711f
NatRousseau <nathan.rousseau@viacesi.fr>
2020-12-17 22:56:47 +0100

    UnitTest Index + CI/CD

--- Test-Unitaire/UnitTest1.cs
+++ Test-Unitaire/UnitTest1.cs
@@ -1,4 +1,9 @@
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CICD_API.Controller;
+using CICD_API;
+using CICD_API.Entity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Test_Unitaire
 {
@@ -6,9 +11,19 @@ namespace Test_Unitaire
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void IndexTest()
         {
-            Assert.IsTrue();
+            GamesController Gc = new GamesController();
+            Assert.IsNotNull(Gc.Index());
         }
+
+        /*[TestMethod]
+        public void CreateTest()
+        {
+            GamesController Gc = new GamesController();
+            var Game = new Game();
+            var result = Gc.Create(Game);
+            Assert.IsInstanceOfType(result,typeof(ActionResult<IEnumerable<GamesController>>));
+        }*/
     }
 }
